c534739d92e1d187d9fba4804c516b29d9e6c07a
Ian Blumel <ian__DOT__blumel_AT_gmail_DOT_com>
2009-10-10 22:18:48 -0700

    FIX: mingw batch file now creates release and debug configurations. FIX: all executable build output now goes into a "bin" directory.

--- build/cmake-mingw.bat
+++ build/cmake-mingw.bat
@@ -3,6 +3,21 @@ SETLOCAL
 SET _OUT=mingw
 IF NOT EXIST %_OUT%\. MKDIR %_OUT%
 PUSHD %_OUT%
-cmake -G"MinGW Makefiles" ..\..\ 
+
+SET _CONFIG=Debug
+ECHO Generating %_CONFIG% configuration
+IF NOT EXIST %_CONFIG%\. MKDIR %_CONFIG%
+PUSHD %_CONFIG%
+cmake -G"MinGW Makefiles" ..\..\..\ -DCMAKE_BUILD_TYPE=%_CONFIG%
+POPD
+
+SET _CONFIG=Release
+ECHO Generating %_CONFIG% configuration
+IF NOT EXIST %_CONFIG%\. MKDIR %_CONFIG%
+PUSHD %_CONFIG%
+cmake -G"MinGW Makefiles" ..\..\..\ -DCMAKE_BUILD_TYPE=%_CONFIG%
+POPD
+
+
 POPD 
 ENDLOCAL
