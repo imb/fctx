7cbd4e0a5c84abd6c23672e4e87ecab24b70c00a
Ian Blumel (OpenPGP Key for Launchpad) <ian.blumel@gmail.com>
2010-07-06 16:23:11 -0700

    Add missing 'empty' test program.

---
+++ tests/test_empty.c
@@ -0,0 +1,18 @@
+/*
+====================================================================
+Copyright (c) 2008 Ian Blumel.  All rights reserved.
+
+This software is licensed as described in the file LICENSE, which
+you should have received as part of this distribution.
+====================================================================
+File: test_basic.c
+
+Provides a dumping ground for basic tests of fct.
+*/
+
+#include "fct.h"
+
+FCT_BGN()
+{
+}
+FCT_END();
