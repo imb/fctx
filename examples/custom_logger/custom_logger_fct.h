6847e23607a721da6d77983b9b3a88611af0c83f
Ian Blumel (OpenPGP Key for Launchpad) <ian.blumel@gmail.com>
2010-07-14 16:18:50 -0700

    Skeleton custom logger implementation.
    Defines all the hooks, but doesn't do anything with them. Next
    step will have each one parse the logger event and display the
    results.

--- examples/custom_logger/custom_logger_fct.h
+++ examples/custom_logger/custom_logger_fct.h
@@ -13,7 +13,7 @@ File: custom_logger_fct.h
 #include "fct.h"
 
 /* Define our custom logger object. */
-struct _cust_logger_t {
+struct _custlog_t {
      /* Define the common logger header. */
      _fct_logger_head;
      /* Add any data members you want to maintain here. Perhaps
@@ -23,7 +23,7 @@ struct _cust_logger_t {
 
 /* Handles what to do when a fct_chk is made. */
 static void
-cust_logger__on_chk(fct_logger_i *l, fct_logger_evt_t const *e) {
+custlog__on_chk(fct_logger_i *l, fct_logger_evt_t const *e) {
     fct_unused(l);
     fctchk_t const *chk = e->chk;
     printf("on_chk: %s (%s)\n"
@@ -37,17 +37,74 @@ cust_logger__on_chk(fct_logger_i *l, fct_logger_evt_t const *e) {
    );
 }
 
+static void
+custlog__on_test_start(fct_logger_i *, fct_logger_evt_t const *e) {
+}
+
+
+static void
+custlog__on_test_end(fct_logger_i *, fct_logger_evt_t const *e) {
+}
+
+static void
+custlog__on_test_start(fct_logger_i *, fct_logger_evt_t const *e) {
+}
+
+static void
+custlog__on_test_suite_start(fct_logger_i *, fct_logger_evt_t const *e) {
+}
+
+static void
+custlog__on_test_suite_end(fct_logger_i *, fct_logger_evt_t const *e) {
+}
+
+static void
+custlog__on_fctx_start(fct_logger_i *, fct_logger_evt_t const *e) {
+}
+
+static void
+custlog__on_fctx_end(fct_logger_i *, fct_logger_evt_t const *e) {
+}
+
+static void
+custlog__on_warn(fct_logger_i *, fct_logger_evt_t const *e) {
+}
+
+static void
+custlog__on_test_suite_skip(fct_logger_i *, fct_logger_evt_t const *e) {
+}
+
+static void
+custlog__on_test_skip(fct_logger_i *, fct_logger_evt_t const *e) {
+}
+
+static void
+custlog__on_delete(fct_logger_i *, fct_logger_evt_t const *e) {
+}
+
 
 static fct_logger_i*
-cust_logger_new(void) {
-   struct _cust_logger_t *logger = (struct _cust_logger_t*)malloc(
-					 sizeof(struct _cust_logger_t)
+custlog_new(void) {
+   struct _custlog_t *logger = (struct _custlog_t*)malloc(
+					 sizeof(struct _custlog_t)
                                  );
   if ( logger == NULL ) {
      return NULL;
   }
   fct_logger__init((fct_logger_i*)logger);
-  logger->vtable.on_chk = cust_logger__on_chk;
+  /* The destructor. */
+  logger->vtable.on_delete = custlog__on_delete;
+  /* The event handlers. */
+  logger->vtable.on_chk = custlog__on_chk;
+  logger->vtable.on_test_start = custlog__on_test_start;
+  logger->vtable.on_test_end = custlog__on_test_end;
+  logger->vtable.on_test_suite_start = custlog__on_test_suite_start;
+  logger->vtable.on_test_suite_end = custlog__on_test_suite_end;
+  logger->vtable.on_fctx_start = custlog__on_fctx_start;
+  logger->vtable.on_fctx_end = custlog__on_fctx_end;
+  logger->vtable.on_warn = custlog_on_warn;
+  logger->vtable.on_test_suite_skip =  custlog__on_test_suite_skip;
+  logger->vtable.on_test_skip = custlog__on_test_skip;
   return (fct_logger_i*)logger;
 }
 
@@ -57,8 +114,8 @@ cust_logger_new(void) {
 
 /* Define how to install the custom logger. To override the built in
 ones use the same name (i.e. "standard" to override the default logger). */
-static fct_logger_types_t cust_loggers[] = {
-   {"standard", (fct_logger_new_fn)cust_logger_new},
+static fct_logger_types_t custlogs[] = {
+   {"custlog", (fct_logger_new_fn)custlog_new},
    {NULL, (fct_logger_new_fn)NULL} /* Sentinel */
 };
 
@@ -67,6 +124,6 @@ static fct_logger_types_t cust_loggers[] = {
 logger. Define the _END macro for symmetry. */
 #define CL_FCT_BGN()                 \
    FCT_BGN() {                       \
-      fctlog_install(cust_loggers);
+      fctlog_install(custlogs);
 
 #define CL_FCT_END() } FCT_END()
