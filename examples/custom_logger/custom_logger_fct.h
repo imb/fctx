1c516f323ce9bdf107e955f0286de5876679d7d6
Ian Blumel (OpenPGP Key for Launchpad) <ian.blumel@gmail.com>
2010-07-20 16:07:07 -0700

    Run astyle on custom_logger.

--- examples/custom_logger/custom_logger_fct.h
+++ examples/custom_logger/custom_logger_fct.h
@@ -13,17 +13,19 @@ File: custom_logger_fct.h
 #include "fct.h"
 
 /* Define our custom logger object. */
-struct _custlog_t {
-     /* Define the common logger header. */
-     _fct_logger_head;
-     /* Add any data members you want to maintain here. Perhaps
-     you want to track something from event to event? */
+struct _custlog_t
+{
+    /* Define the common logger header. */
+    _fct_logger_head;
+    /* Add any data members you want to maintain here. Perhaps
+    you want to track something from event to event? */
 };
 
 
 /* Handles what to do when a fct_chk is made. */
 static void
-custlog__on_chk(fct_logger_i *l, fct_logger_evt_t const *e) {
+custlog__on_chk(fct_logger_i *l, fct_logger_evt_t const *e)
+{
     fct_unused(l);
     fctchk_t const *chk = e->chk;
     printf("on_chk: %s\n"
@@ -35,15 +37,17 @@ custlog__on_chk(fct_logger_i *l, fct_logger_evt_t const *e) {
            fctchk__lineno(chk),
            fctchk__msg(chk),
            fctchk__cndtn(chk) /* This should be deprecated. */
-   );
+          );
 }
 
 
 
 /* Handles the start of a test, for example FCT_TEST_BGN(). */
 static void
-custlog__on_test_start(fct_logger_i *l, fct_logger_evt_t const *e) {
-    (void)l; (void)e;
+custlog__on_test_start(fct_logger_i *l, fct_logger_evt_t const *e)
+{
+    (void)l;
+    (void)e;
     fct_test_t const *test = e->test;
     printf("on_test_start:\n"
            "    -      name: %s\n",
@@ -53,7 +57,8 @@ custlog__on_test_start(fct_logger_i *l, fct_logger_evt_t const *e) {
 
 /* Handles the end of a test, for example FCT_TEST_END(). */
 static void
-custlog__on_test_end(fct_logger_i *l, fct_logger_evt_t const *e) {
+custlog__on_test_end(fct_logger_i *l, fct_logger_evt_t const *e)
+{
     (void)l;
     fct_test_t const *test = e->test;
     printf("on_test_end:\n"
@@ -66,24 +71,26 @@ custlog__on_test_end(fct_logger_i *l, fct_logger_evt_t const *e) {
 
 /* Handles the start of a test suite, for example FCT_TESTSUITE_BGN(). */
 static void
-custlog__on_test_suite_start(fct_logger_i *l, fct_logger_evt_t const *e) {
-   (void)l;
-   fct_ts_t const *test_suite = e->ts;
-   printf("on_test_suite_start:\n"
+custlog__on_test_suite_start(fct_logger_i *l, fct_logger_evt_t const *e)
+{
+    (void)l;
+    fct_ts_t const *test_suite = e->ts;
+    printf("on_test_suite_start:\n"
            "    -      name: %s\n",
            fct_ts__name(test_suite)
-         );
+          );
 }
 
 /* Handles the end of a test suite, for example FCT_TESTSUITE_END(). */
 static void
-custlog__on_test_suite_end(fct_logger_i *l, fct_logger_evt_t const *e) {
-   fct_ts_t const *test_suite = e->ts;
-   int test_cnt = fct_ts__tst_cnt(test_suite);
-   int passed_test_cnt = fct_ts__tst_cnt(test_suite);
-   int failed_test_cnt = test_cnt - passed_test_cnt;
-   (void)l;
-   printf("on_test_suite_end:\n"
+custlog__on_test_suite_end(fct_logger_i *l, fct_logger_evt_t const *e)
+{
+    fct_ts_t const *test_suite = e->ts;
+    int test_cnt = fct_ts__tst_cnt(test_suite);
+    int passed_test_cnt = fct_ts__tst_cnt(test_suite);
+    int failed_test_cnt = test_cnt - passed_test_cnt;
+    (void)l;
+    printf("on_test_suite_end:\n"
            "    -          name: %s\n"
            "    -      duration: %f ms\n"
            "    -  tests passed: %d\n"
@@ -94,69 +101,75 @@ custlog__on_test_suite_end(fct_logger_i *l, fct_logger_evt_t const *e) {
            passed_test_cnt,
            failed_test_cnt,
            fct_ts__chk_cnt(test_suite)
-         );
+          );
 }
 
 /* Handles the first time FCTX can offically say 'start'. */
 static void
-custlog__on_fctx_start(fct_logger_i *l, fct_logger_evt_t const *e) {
-    (void)l; 
+custlog__on_fctx_start(fct_logger_i *l, fct_logger_evt_t const *e)
+{
+    (void)l;
     (void)e;
     printf("on_fctx_start:\n");
 }
 
 /* Handles the last time FCTX can do anything. */
 static void
-custlog__on_fctx_end(fct_logger_i *l, fct_logger_evt_t const *e) {
-    (void)l; 
+custlog__on_fctx_end(fct_logger_i *l, fct_logger_evt_t const *e)
+{
+    (void)l;
     (void)e;
-    printf("on_fctx_end:\n"); 
+    printf("on_fctx_end:\n");
 }
 
 /* Handles a warning message produced by FCTX. */
 static void
-custlog__on_warn(fct_logger_i *l, fct_logger_evt_t const *e) {
-   (void)l;
-   char const *message = e->msg;
-   printf("on_warn: %s\n", message);
+custlog__on_warn(fct_logger_i *l, fct_logger_evt_t const *e)
+{
+    (void)l;
+    char const *message = e->msg;
+    printf("on_warn: %s\n", message);
 }
 
 /* When a conditional test suite is skipped due to conditional evaluation. */
 static void
-custlog__on_test_suite_skip(fct_logger_i *l, fct_logger_evt_t const *e) { 
-   (void)l; 
-   /* Conditional evaluation on why we skipped. */
-   const char *condition = e->cndtn;
-   /* Name of test suite skipped. */
-   const char *name = e->name;   
-   printf("on_test_suite_skip:\n"
-          "    -      name: %s\n"
-          "    - condition: %s\n",
-          name,
-          condition
-         );
+custlog__on_test_suite_skip(fct_logger_i *l, fct_logger_evt_t const *e)
+{
+    (void)l;
+    /* Conditional evaluation on why we skipped. */
+    const char *condition = e->cndtn;
+    /* Name of test suite skipped. */
+    const char *name = e->name;
+    printf("on_test_suite_skip:\n"
+           "    -      name: %s\n"
+           "    - condition: %s\n",
+           name,
+           condition
+          );
 }
 
 /* When a conditional test is skipped due to conditional evaluation. */
 static void
-custlog__on_test_skip(fct_logger_i *l, fct_logger_evt_t const *e) {
-   (void)l; 
-   /* Conditional evaluation on why we skipped. */
-   const char *condition = e->cndtn;
-   /* Name of test suite skipped. */
-   const char *name = e->name;   
-   printf("on_test_suite_skip:\n"
-          "    -      name: %s\n"
-          "    - condition: %s\n",
-          name,
-          condition
-         );
+custlog__on_test_skip(fct_logger_i *l, fct_logger_evt_t const *e)
+{
+    (void)l;
+    /* Conditional evaluation on why we skipped. */
+    const char *condition = e->cndtn;
+    /* Name of test suite skipped. */
+    const char *name = e->name;
+    printf("on_test_suite_skip:\n"
+           "    -      name: %s\n"
+           "    - condition: %s\n",
+           name,
+           condition
+          );
 }
 
 /* Handles the clean up of the logger object. Perform your special
 clean up code here. */
 static void
-custlog__on_delete(fct_logger_i *l, fct_logger_evt_t const *e) {
+custlog__on_delete(fct_logger_i *l, fct_logger_evt_t const *e)
+{
     (void)e; /* Currently doesn't supply anything. */
     puts("custlog__on_delete:\n");
     free((struct _custlog_t*)l);
@@ -164,28 +177,30 @@ custlog__on_delete(fct_logger_i *l, fct_logger_evt_t const *e) {
 
 
 static fct_logger_i*
-custlog_new(void) {
-   struct _custlog_t *logger = (struct _custlog_t*)malloc(
-					 sizeof(struct _custlog_t)
-                                 );
-  if ( logger == NULL ) {
-     return NULL;
-  }
-  fct_logger__init((fct_logger_i*)logger);
-  /* The destructor. */
-  logger->vtable.on_delete = custlog__on_delete;
-  /* The event handlers. */
-  logger->vtable.on_chk = custlog__on_chk;
-  logger->vtable.on_test_start = custlog__on_test_start;
-  logger->vtable.on_test_end = custlog__on_test_end;
-  logger->vtable.on_test_suite_start = custlog__on_test_suite_start;
-  logger->vtable.on_test_suite_end = custlog__on_test_suite_end;
-  logger->vtable.on_fctx_start = custlog__on_fctx_start;
-  logger->vtable.on_fctx_end = custlog__on_fctx_end;
-  logger->vtable.on_warn = custlog__on_warn;
-  logger->vtable.on_test_suite_skip =  custlog__on_test_suite_skip;
-  logger->vtable.on_test_skip = custlog__on_test_skip;
-  return (fct_logger_i*)logger;
+custlog_new(void)
+{
+    struct _custlog_t *logger = (struct _custlog_t*)malloc(
+                                    sizeof(struct _custlog_t)
+                                );
+    if ( logger == NULL )
+    {
+        return NULL;
+    }
+    fct_logger__init((fct_logger_i*)logger);
+    /* The destructor. */
+    logger->vtable.on_delete = custlog__on_delete;
+    /* The event handlers. */
+    logger->vtable.on_chk = custlog__on_chk;
+    logger->vtable.on_test_start = custlog__on_test_start;
+    logger->vtable.on_test_end = custlog__on_test_end;
+    logger->vtable.on_test_suite_start = custlog__on_test_suite_start;
+    logger->vtable.on_test_suite_end = custlog__on_test_suite_end;
+    logger->vtable.on_fctx_start = custlog__on_fctx_start;
+    logger->vtable.on_fctx_end = custlog__on_fctx_end;
+    logger->vtable.on_warn = custlog__on_warn;
+    logger->vtable.on_test_suite_skip =  custlog__on_test_suite_skip;
+    logger->vtable.on_test_skip = custlog__on_test_skip;
+    return (fct_logger_i*)logger;
 }
 
 
@@ -194,11 +209,12 @@ custlog_new(void) {
 
 /* Define how to install the custom logger. To override the built in
 ones use the same name (i.e. "standard" to override the default logger). */
-static fct_logger_types_t custlogs[] = {
-   {"custlog", (fct_logger_new_fn)custlog_new,
-   "custom logger example, outputs everything!"
-   },
-   {NULL, (fct_logger_new_fn)NULL, NULL} /* Sentinel */
+static fct_logger_types_t custlogs[] =
+{
+    {"custlog", (fct_logger_new_fn)custlog_new,
+        "custom logger example, outputs everything!"
+    },
+    {NULL, (fct_logger_new_fn)NULL, NULL} /* Sentinel */
 };
 
 
